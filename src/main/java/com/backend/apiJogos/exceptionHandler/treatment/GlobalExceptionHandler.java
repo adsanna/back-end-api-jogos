518ed1e8d5a8bac9e5a0e88fccd27b2f7e3e5ed0
grazielle diniz <graziellediniz2007@gmail.com>
2026-06-10 21:19:21 -0300

    fix: padronizando exceptions

--- src/main/java/com/backend/apiJogos/exceptionHandler/treatment/GlobalExceptionHandler.java
+++ src/main/java/com/backend/apiJogos/exceptionHandler/treatment/GlobalExceptionHandler.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.backend.apiJogos.exceptionHandler.exceptions.UserJaCadastradoException;
+import com.backend.apiJogos.exceptionHandler.exceptions.UserAlreadyRegisteredException;
 import com.backend.apiJogos.exceptionHandler.exceptions.UserNotFoundException;
 import com.backend.apiJogos.exceptionHandler.exceptions.AccessDeniedException;
 import com.backend.apiJogos.exceptionHandler.exceptions.GameNotFoundException;
 import com.backend.apiJogos.exceptionHandler.exceptions.InvalidUserDataException;
-import com.backend.apiJogos.exceptionHandler.exceptions.RunEmAndamentoException;
+import com.backend.apiJogos.exceptionHandler.exceptions.RunInProgressException;
 
 import com.backend.apiJogos.exceptionHandler.formatter.RestErrorMensagem;
 
@@ -24,8 +24,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     return ResponseEntity.status(status).body(new RestErrorMensagem(status.value(), mensagem));
   }
 
-  @ExceptionHandler(UserJaCadastradoException.class)
-  ResponseEntity<RestErrorMensagem> UserJaCadastradoExcepition(UserJaCadastradoException ex) {
+  @ExceptionHandler(UserAlreadyRegisteredException.class)
+  ResponseEntity<RestErrorMensagem> UserAlreadyRegisteredException(UserAlreadyRegisteredException ex) {
     return criarResposta(HttpStatus.CONFLICT, ex.getMessage());
   }
 
@@ -49,26 +49,26 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     return criarResposta(HttpStatus.INTERNAL_SERVER_ERROR, "Erro interno do servidor");
   }
 
-  @ExceptionHandler(RunEmAndamentoException.class)
-  ResponseEntity<RestErrorMensagem> RunEmAndamentoException(RunEmAndamentoException ex) {
+  @ExceptionHandler(RunInProgressException.class)
+  ResponseEntity<RestErrorMensagem> RunInProgressException(RunInProgressException ex) {
     return criarResposta(HttpStatus.CONFLICT, ex.getMessage());
   }
 
-  @ExceptionHandler(com.backend.apiJogos.exceptionHandler.exceptions.RunNaoEncontradaException.class)
-  ResponseEntity<RestErrorMensagem> RunNaoEncontradaException(
-      com.backend.apiJogos.exceptionHandler.exceptions.RunNaoEncontradaException ex) {
+  @ExceptionHandler(com.backend.apiJogos.exceptionHandler.exceptions.RunNotFoundException.class)
+  ResponseEntity<RestErrorMensagem> RunNotFoundException(
+      com.backend.apiJogos.exceptionHandler.exceptions.RunNotFoundException ex) {
     return criarResposta(HttpStatus.NOT_FOUND, ex.getMessage());
   }
 
-  @ExceptionHandler(com.backend.apiJogos.exceptionHandler.exceptions.HorasException.class)
-  ResponseEntity<RestErrorMensagem> HorasException(
-      com.backend.apiJogos.exceptionHandler.exceptions.HorasException ex) {
+  @ExceptionHandler(com.backend.apiJogos.exceptionHandler.exceptions.TimeException.class)
+  ResponseEntity<RestErrorMensagem> TimeException(
+      com.backend.apiJogos.exceptionHandler.exceptions.TimeException ex) {
     return criarResposta(HttpStatus.CONFLICT, ex.getMessage());
   }
 
-  @ExceptionHandler(com.backend.apiJogos.exceptionHandler.exceptions.DatasException.class)
-  ResponseEntity<RestErrorMensagem> DatasException(
-      com.backend.apiJogos.exceptionHandler.exceptions.DatasException ex) {
+  @ExceptionHandler(com.backend.apiJogos.exceptionHandler.exceptions.DateException.class)
+  ResponseEntity<RestErrorMensagem> DateException(
+      com.backend.apiJogos.exceptionHandler.exceptions.DateException ex) {
     return criarResposta(HttpStatus.CONFLICT, ex.getMessage());
   }
 
